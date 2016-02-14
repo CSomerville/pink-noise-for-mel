34dfbbbe5803a61a6064e80b2d02a240143c71ad
Colby Somerville <colby.somerville@gmail.com>
2016-02-14 16:44:42 -0500

    adds oscilloscope

--- public/main.js
+++ public/main.js
@@ -1,13 +1,21 @@
+// the pink noise implementation is Zach Denton's:
+// http://noisehack.com/generate-noise-web-audio-api/
+
+
 window.onload = init;
 
 function init() {
   mel = noisy();
   mel.sing();
+  mel.animate();
 }
 
 function noisy() {
   var audioCtx = audioCtx || new AudioContext;
   var bufferSize = 4096;
+
+  var canvas = document.getElementById('oscilloscope');
+  var toAnimate = [];
 
   var pinkNoise = function pinkNoise() {
     var b0, b1, b2, b3, b4, b5, b6;
@@ -32,10 +40,72 @@ function noisy() {
     return node;
   }
 
+  var oscilloscopeInit = function oscilloscopeInit(canvas) {
+    // takes canvas DOM element. Returns a web audio analyzer node as well
+    // as a draw function.
+    var analyzer, bufferLength, dataArray, canvasCtx;
+
+    analyzer = audioCtx.createAnalyser();
+    analyzer.fftsize = 4096;
+    bufferLength = analyzer.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength);
+
+    canvasCtx = canvas.getContext('2d');
+
+    var draw = function draw() {
+      var sliceWidth, x, y, v;
+
+      analyzer.getByteTimeDomainData(dataArray);
+
+      // clear canvas
+      canvasCtx.fillStyle = 'pink';
+      canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+      // init draw
+      canvasCtx.lineWidth = 2;
+      canvasCtx.strokeStyle = 'black';
+      canvasCtx.beginPath();
+
+      sliceWidth = canvas.width * 1.0 / bufferLength;
+      x = 0;
+
+      for (var i = 0; i < bufferLength; i++) {
+        v = dataArray[i] / 128.0;
+        y = v * canvas.height - canvas.height / 2;
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width, canvas.height/2);
+      canvasCtx.stroke();
+    }
+
+    return {
+      analyzer: analyzer,
+      draw: draw
+    }
+  }
+
   return {
     sing: function sing() {
       var pink = pinkNoise();
+      var oscilloscope = oscilloscopeInit(canvas);
+      toAnimate.push(oscilloscope.draw);
+
+      pink.connect(oscilloscope.analyzer);
       pink.connect(audioCtx.destination);
+    },
+
+    animate: function animate() {
+      requestAnimationFrame(animate);
+      for (var i = 0; i < toAnimate.length; i++) {
+        toAnimate[i]();
+      }
     }
   }
 }
